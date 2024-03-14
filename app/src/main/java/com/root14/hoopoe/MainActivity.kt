25b1280804cb5a1b564a629e6658df2b15aafce3
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2024-03-14 21:47:36 +0300

    add main screen view

--- app/src/main/java/com/root14/hoopoe/MainActivity.kt
+++ app/src/main/java/com/root14/hoopoe/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.root14.hoopoe
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.root14.hoopoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.progressIndicator.isIndeterminate = false
     }
 }
---
+++ app/src/main/java/com/root14/hoopoe/view/adapter/CoinRecycleAdapter.kt
@@ -0,0 +1,26 @@
+package com.root14.hoopoe.view.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CoinRecycleAdapter : RecyclerView.Adapter<CoinRecycleAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CoinRecycleAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CoinRecycleAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
