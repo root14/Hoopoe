4cbd408cfce4a64da10860e1301b01df057ab9a4
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2024-03-26 21:42:43 +0300

    add bottomsheetsearch to transaction

--- app/src/main/java/com/root14/hoopoe/view/adapter/MainBottomSheetSearchAdapter.kt
+++ app/src/main/java/com/root14/hoopoe/view/adapter/MainBottomSheetSearchAdapter.kt
@@ -1,8 +1,10 @@
 package com.root14.hoopoe.view.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.root14.hoopoe.AssetDetailActivity
 import com.root14.hoopoe.data.model.Assets
 import com.root14.hoopoe.data.model.AssetsData
 import com.root14.hoopoe.databinding.RwCoinBinding
@@ -25,6 +27,17 @@ class MainBottomSheetSearchAdapter(private val assets: Assets) :
     override fun onBindViewHolder(holder: MainBottomSheetSearchViewHolder, position: Int) {
         val coin = assets.data[position]
         holder.binding(coin)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(
+                holder.itemView.context, AssetDetailActivity()::class.java
+            )
+
+            intent.putExtra("assetId", assets.data[position].symbol.toString())
+            intent.putExtra("assetName", assets.data[position].id.toString())
+
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
 }
