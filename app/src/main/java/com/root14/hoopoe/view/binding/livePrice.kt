5953025d962bdff02e5569b5030ad94ac1cf6a56
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2024-04-01 17:28:42 +0300

    add live price update effect

--- app/src/main/java/com/root14/hoopoe/view/binding/livePrice.kt
+++ app/src/main/java/com/root14/hoopoe/view/binding/livePrice.kt
@@ -1,12 +1,32 @@
 package com.root14.hoopoe.view.binding
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.os.Handler
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.root14.hoopoe.utils.convertPriceFormat
 
 @BindingAdapter("setLivePrice")
 fun setLivePrice(textView: TextView, data: String) {
-    if (data.isNotEmpty()) {
-        textView.text = data.convertPriceFormat()
+    val previousPrice = textView.text.drop(1).toString().replace(",", "").toDoubleOrNull() ?: 0.0
+
+    if (data.isNotEmpty() and textView.text.isNotEmpty()) {
+        val textColorAnimator = ObjectAnimator.ofArgb(
+            textView,
+            "textColor",
+            textView.context.getColor(if (previousPrice < data.toDouble()) android.R.color.holo_green_dark else android.R.color.holo_red_dark),
+            textView.context.getColor(android.R.color.black)
+        ).apply {
+            setEvaluator(ArgbEvaluator())
+            duration = 1000
+        }
+
+        if (textView.text.isNotEmpty()) {
+            textView.postDelayed({
+                textView.text = data.convertPriceFormat()
+                textColorAnimator.start()
+            }, 1000)
+        }
     }
 }
